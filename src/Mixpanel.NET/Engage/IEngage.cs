2d4584a4975914d5115d7499857e547aac031a2c
Tadeu Maia <tadeumaia@gmail.com>
2013-04-09 20:04:50 -0300

    Explicitly set IP address on engage outside $set scope.

--- src/Mixpanel.NET/Engage/IEngage.cs
+++ src/Mixpanel.NET/Engage/IEngage.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace Mixpanel.NET.Engage {
   public interface IEngage {
-    bool Set(string distinctId, IDictionary<string, object> setProperties);
-    bool Increment(string distinctId, IDictionary<string, object> incrementProperties);
+    bool Set(string distinctId, IDictionary<string, object> setProperties, string ip);
+    bool Increment(string distinctId, IDictionary<string, object> incrementProperties, string ip);
   }
 }
