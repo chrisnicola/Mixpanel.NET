88a22ec38ee85a45a6a97f4fa532004b6f96433c
Tom Goff <tom@codenaked.com>
2015-10-14 14:24:49 -0400

    Code formatting only, no functional changes.

--- src/Mixpanel.NET/Engage/MixpanelEngage.cs
+++ src/Mixpanel.NET/Engage/MixpanelEngage.cs
@@ -1,61 +1,71 @@
 using System.Collections.Generic;
 using System.Web.Script.Serialization;
 
-namespace Mixpanel.NET.Engage {
-  public class MixpanelEngage : MixpanelClientBase, IEngage {
-    private readonly EngageOptions _options;
+namespace Mixpanel.NET.Engage
+{
+	public class MixpanelEngage : MixpanelClientBase, IEngage
+	{
+		private readonly EngageOptions _options;
 
-    /// <summary>
-    /// Creates a new Mixpanel Engage client for a given API token
-    /// </summary>
-    /// <param name="token">The API token for MixPanel</param>
-    /// <param name="http">Optional: An implementation of IMixpanelHttp, <see cref="MixpanelHttp"/>
-    /// Determines if class names and properties will be serialized to JSON literally.
-    /// If false (the default) spaces will be inserted between camel-cased words for improved 
-    /// readability on the reporting side.
-    /// </param>
-    /// <param name="options">Optional: Specific options for the API <see cref="EngageOptions"/></param>
-    public MixpanelEngage(string token, IMixpanelHttp http = null, EngageOptions options = null)
-      : base(token, http) {
-      _options = options ?? new EngageOptions();
-    }
+		/// <summary>
+		/// Creates a new Mixpanel Engage client for a given API token
+		/// </summary>
+		/// <param name="token">The API token for MixPanel</param>
+		/// <param name="http">Optional: An implementation of IMixpanelHttp, <see cref="MixpanelHttp"/>
+		/// Determines if class names and properties will be serialized to JSON literally.
+		/// If false (the default) spaces will be inserted between camel-cased words for improved 
+		/// readability on the reporting side.
+		/// </param>
+		/// <param name="options">Optional: Specific options for the API <see cref="EngageOptions"/></param>
+		public MixpanelEngage(string token, IMixpanelHttp http = null, EngageOptions options = null)
+			: base(token, http)
+		{
+			_options = options ?? new EngageOptions();
+		}
 
-    private bool Engage(string distinctId, IDictionary<string, object> setProperties = null,
-      IDictionary<string, object> incrementProperties = null, bool delete = false, string ip = null) {
-      // Standardize token and time values for Mixpanel
-      var dictionary = 
-        new Dictionary<string, object> {{"$token", token}, {"$distinct_id", distinctId}};
+		private bool Engage(string distinctId, IDictionary<string, object> setProperties = null,
+		  IDictionary<string, object> incrementProperties = null, bool delete = false, string ip = null)
+		{
+			// Standardize token and time values for Mixpanel
+			var dictionary = new Dictionary<string, object> { { "$token", token }, { "$distinct_id", distinctId } };
 
-      if (!string.IsNullOrWhiteSpace(ip)) dictionary.Add("$ip",ip);
+			if (!string.IsNullOrWhiteSpace(ip))
+				dictionary.Add("$ip", ip);
 
-      if (setProperties != null) dictionary.Add("$set", setProperties.FormatProperties());
+			if (setProperties != null)
+				dictionary.Add("$set", setProperties.FormatProperties());
 
-      if (incrementProperties != null) dictionary.Add("$add", incrementProperties.FormatProperties());
+			if (incrementProperties != null)
+				dictionary.Add("$add", incrementProperties.FormatProperties());
 
-      if (delete) dictionary.Add("$delete", string.Empty);
+			if (delete)
+				dictionary.Add("$delete", string.Empty);
 
-      var data = new JavaScriptSerializer().Serialize(dictionary);
+			var data = new JavaScriptSerializer().Serialize(dictionary);
 
-      var values = "data=" + data.Base64Encode();
+			var values = "data=" + data.Base64Encode();
 
-      var contents = _options.UseGet
-        ? http.Get(Resources.Engage(_options.ProxyUrl), values)
-        : http.Post(Resources.Engage(_options.ProxyUrl), values);
+			var contents = _options.UseGet
+			  ? http.Get(Resources.Engage(_options.ProxyUrl), values)
+			  : http.Post(Resources.Engage(_options.ProxyUrl), values);
 
-      return contents == "1";
-    }
+			return contents == "1";
+		}
 
-      public bool Delete(string distinctId) {
-      return Engage(distinctId, delete: true);
-    }
+		public bool Delete(string distinctId)
+		{
+			return Engage(distinctId, delete: true);
+		}
 
-    public bool Set(string distinctId, IDictionary<string, object> setProperties, string ip = null) {
-      return Engage(distinctId, setProperties, ip: ip);
-    }
+		public bool Set(string distinctId, IDictionary<string, object> setProperties, string ip = null)
+		{
+			return Engage(distinctId, setProperties, ip: ip);
+		}
 
-    public bool Increment(string distinctId, IDictionary<string, object> incrementProperties, string ip = null) {
-      return Engage(distinctId, incrementProperties: incrementProperties, ip: ip);
-    }
+		public bool Increment(string distinctId, IDictionary<string, object> incrementProperties, string ip = null)
+		{
+			return Engage(distinctId, incrementProperties: incrementProperties, ip: ip);
+		}
 
-  }
+	}
 }
